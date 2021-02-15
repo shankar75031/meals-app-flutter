a0778f8d75653a87b536e62c033c092116c9a2a4
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-02-16 01:33:07 +0530

    added some styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:homely_meals/widgets/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,31 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Homely Meals'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(
-          'You have pushed the button this many times:',
-        ),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
--- lib/widgets/categories_screen.dart
+++ lib/widgets/categories_screen.dart
@@ -5,15 +5,20 @@ import 'package:homely_meals/widgets/category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((category) {
-        return CategoryItem(category.title, category.color);
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Homely Meals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(category.title, category.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
